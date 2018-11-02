53338d4c26c3236fd3d1a5eb9bae15b7de9e80bd
Fendrel- <dswift88@gmail.com>
2018-11-01 22:29:32 -0700

    Added search menu items.

--- work_log.py
+++ work_log.py
@@ -2,12 +2,19 @@ import os
 
 import classes
 
+
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
+def find_by_date():
+    header('Find by Date')
+
 def find_by_pattern():
-    clear()
-    input('This is a placeholder for a function. (Press enter)')
+    header('Find by Pattern')
+    input(' This is a placeholder for a function. (Press enter)')
+
+def find_by_exact():
+    header('Find by Exact Name')
 
 def header(heading):
     clear()
@@ -18,15 +25,23 @@ def header(heading):
 def menu_prompt():
     return input('\n Choose an option: ')
 
+def display_entries():
+    pass
+
 def search_menu():
     status_message = None
     while True:
         header('Search Entries')
         classes.Menu(status_message, "Find By Date", "Find By Time Spent", "Find By Exact Name", "Find By Pattern", "Find By Telekinesis")
         menu_choice = menu_prompt()
+        if menu_choice == "1":
+            find_by_date()
+        if menu_choice == "2":
+            pass
+        if menu_choice == "3":
+            find_by_exact()
         if menu_choice == "4":
             find_by_pattern()
-            menu = None
         if menu_choice == "5":
             status_message = 'Nah, I\'m just kidding, don\'t\n bend the spoon, that\'s impossible!\n\n Achievement unlocked\n 250G - Neo != The One'
         if menu_choice == "6":
@@ -48,10 +63,12 @@ def top_menu():
     status_message = None
     while True:
         header('Main Menu')
-        classes.MainMenu(status_message, 'Add New Entry', 'Search Entries')
+        classes.MainMenu(status_message, 'Add New Entry', 'Show All Entries', 'Search Entries')
         menu_choice = menu_prompt()
         if menu_choice == "1":
             status_message = add_entry()
+        if menu_choice == "2":
+            status_message = display_entries()
         elif menu_choice == "2":
             status_message = search_menu()
         elif menu_choice == "3":
